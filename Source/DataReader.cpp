176c468952a4da34baa699fe2d3904bcdf6090e7
Nghia Truong <nghiatruong.vn@gmail.com>
2018-05-13 00:42:36 -0600

    :blue_car: fix data reading error

--- Source/DataReader.cpp
+++ Source/DataReader.cpp
@@ -33,19 +33,19 @@ DataReader::DataReader()
                             {
                                 static char buff[512];
                                 __BNN_SPRINT(buff, "%s/%s/%s.%04d.%s", m_DataPath.toStdString().c_str(), "FluidFrame", "frame", fileID, "pos");
-                                return std::string(buff);
+                                return String(buff);
                             };
     m_GenFluidAniMatrixFileName = [&](int fileID)
                                   {
                                       static char buff[512];
                                       __BNN_SPRINT(buff, "%s/%s/%s.%04d.%s", m_DataPath.toStdString().c_str(), "FluidFrame", "frame", fileID, "ani");
-                                      return std::string(buff);
+                                      return String(buff);
                                   };
     m_GenMeshFileName = [&](int fileID)
                         {
                             static char buff[512];
                             __BNN_SPRINT(buff, "%s/%s/%s.%04d.%s", m_DataPath.toStdString().c_str(), "SolidFrame", "frame", fileID, "pos");
-                            return std::string(buff);
+                            return String(buff);
                         };
 
     m_AutoTimer = std::make_unique<QTimer>(this);
@@ -75,7 +75,6 @@ void DataReader::setParticleDataObj(const SharedPtr<ParticleSystemData>& particl
 void DataReader::setMeshObj(const std::vector<SharedPtr<MeshObject>>& meshObj)
 {
     Q_ASSERT(meshObj.size() > 0);
-
     if(m_MeshObj.size() == 0) {
         m_MeshObj = meshObj;
     }
@@ -94,7 +93,6 @@ void DataReader::setNumFrames(int numFrames)
 void DataReader::readNextFrame()
 {
     int nextFrame = m_Reverse ? m_CurrentFrame - m_FrameStep : m_CurrentFrame + m_FrameStep;
-
     if(m_bRepeat) {
         if(nextFrame <= 0) {
             nextFrame = m_NumFrames;
@@ -104,7 +102,6 @@ void DataReader::readNextFrame()
     } else if((nextFrame <= 0) || (nextFrame > m_NumFrames)) {
         return;
     }
-
     readFrame(nextFrame);
 }
 
@@ -114,11 +111,9 @@ void DataReader::readFrame(int frame)
     if(m_ParticleData == nullptr || m_DataInfo == nullptr || frame == m_CurrentFrame) {
         return;
     }
-
     m_ReadBytes = 0;
 
     bool successReadFluid = readFluidPosition(frame);
-
     if(successReadFluid && m_bUseAnisotropyKernel) {
         successReadFluid = readFluidAnisotropyKernel(frame);
     }
@@ -142,8 +137,6 @@ void DataReader::readFrame(int frame)
             m_ParticleData->setNumParticles(m_DataInfo->num_particles);
             m_ParticleData->setUInt("ColorRandomReady", 0);
             m_ParticleData->setUInt("ColorRampReady",   0);
-            //            m_ParticleData->setUInt("AnisotrpyMatrixReady", 0);
-
             emit numParticlesChanged(m_DataInfo->num_particles);
         }
     }
@@ -152,25 +145,23 @@ void DataReader::readFrame(int frame)
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 void DataReader::allocateMemory()
 {
-    assert(m_ParticleData != nullptr);
-
+    Q_ASSERT(m_ParticleData != nullptr);
     m_ParticleData->addArray<float, 3>("Position");
     m_ParticleData->addArray<float, 3>("ColorRandom");
     m_ParticleData->addArray<float, 3>("ColorRamp");
     m_ParticleData->addArray<float, 9>("AnisotropyKernelMatrix");
-    m_ParticleData->reserve(1000000);
+    m_ParticleData->reserve(1024);
 }
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 bool DataReader::readFluidPosition(int frameID)
 {
-    std::string   fileName = m_GenFluidPosFileName(frameID);
+    String        fileName = m_GenFluidPosFileName(frameID);
     std::ifstream file(fileName.c_str(), std::ios::binary | std::ios::ate);
-
     if(!file.is_open()) {
         return false;
     }
-
+    ////////////////////////////////////////////////////////////////////////////////
     m_ReadTimer.tick();
     size_t fileSize = (size_t)file.tellg();
     m_VReadBuffer.resize(fileSize);
@@ -180,7 +171,6 @@ bool DataReader::readFluidPosition(int frameID)
     file.close();
     m_ReadBytes += fileSize;
     m_ReadTimer.tock();
-
     ////////////////////////////////////////////////////////////////////////////////
     unsigned int numParticles;
     float        particleRadius;
@@ -199,6 +189,9 @@ bool DataReader::readFluidPosition(int frameID)
 
     if(numParticles != m_DataInfo->num_particles) {
         m_DataInfo->num_particles = numParticles;
+    }
+
+    if(m_ParticleData->getNParticles() != numParticles) {
         m_ParticleData->allocate(numParticles);
     }
 
@@ -225,13 +218,12 @@ bool DataReader::readFluidPosition(int frameID)
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 bool DataReader::readFluidAnisotropyKernel(int frameID)
 {
-    std::string   fileName = m_GenFluidAniMatrixFileName(frameID);
+    String        fileName = m_GenFluidAniMatrixFileName(frameID);
     std::ifstream file(fileName.c_str(), std::ios::binary | std::ios::ate);
-
     if(!file.is_open()) {
         return false;
     }
-
+    ////////////////////////////////////////////////////////////////////////////////
     m_ReadTimer.tick();
     size_t fileSize = (size_t)file.tellg();
     m_VReadBuffer.resize(fileSize);
@@ -241,17 +233,15 @@ bool DataReader::readFluidAnisotropyKernel(int frameID)
     file.close();
     m_ReadBytes += fileSize;
     m_ReadTimer.tock();
-
+    ////////////////////////////////////////////////////////////////////////////////
     unsigned int numParticles;
     memcpy(&numParticles, m_VReadBuffer.data(), sizeof(unsigned int));
-
     Q_ASSERT(numParticles == m_DataInfo->num_particles);
-
+    ////////////////////////////////////////////////////////////////////////////////
     size_t dataSize = 9 * numParticles * sizeof(float);
     Q_ASSERT(dataSize + sizeof(unsigned int) == fileSize);
     memcpy(m_ParticleData->getArray("AnisotropyKernelMatrix")->data(), &m_VReadBuffer.data()[sizeof(unsigned int)], dataSize);
     m_ParticleData->setUInt("AnisotrpyMatrixReady", 1);
-
     ////////////////////////////////////////////////////////////////////////////////
     return true;
 }
@@ -259,13 +249,12 @@ bool DataReader::readFluidAnisotropyKernel(int frameID)
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 bool DataReader::readMeshes(int frameID)
 {
-    std::string   fileName = m_GenMeshFileName(frameID);
+    String        fileName = m_GenMeshFileName(frameID);
     std::ifstream file(fileName.c_str(), std::ios::binary | std::ios::ate);
-
     if(!file.is_open()) {
         return false;
     }
-
+    ////////////////////////////////////////////////////////////////////////////////
     m_ReadTimer.tick();
     size_t fileSize = (size_t)file.tellg();
     m_VReadBuffer.resize(fileSize);
@@ -275,7 +264,6 @@ bool DataReader::readMeshes(int frameID)
     file.close();
     m_ReadBytes += fileSize;
     m_ReadTimer.tock();
-
     ////////////////////////////////////////////////////////////////////////////////
     unsigned int numMeshes = 0;
 #ifdef NEW_DATA
@@ -296,7 +284,7 @@ bool DataReader::readMeshes(int frameID)
         offset += dataSize;
 
         dataSize = 3 * numVertices * sizeof(float);
-        assert(dataSize * 2 + offset <= m_VReadBuffer.size());
+        Q_ASSERT(dataSize * 2 + offset <= m_VReadBuffer.size());
 
         m_MeshObj[i]->setVertices((void*)&m_VReadBuffer.data()[offset], dataSize);
         offset += dataSize;
